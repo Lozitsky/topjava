b586f1f3833478b6f63e67807cf976fca0640ad4
Bohdan Lozitskey <bondxxl01@gmail.com>
2018-07-09 11:03:50 +0300

    HW 02 task 1-2

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -14,7 +14,14 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userNameD", "email1@mail.ru", "password", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userNameA", "email2@mail.ru", "password", Role.ROLE_USER));
+            adminUserController.create(new User(null, "userNameC", "email3@mail.ru", "password", Role.ROLE_USER));
+            adminUserController.create(new User(null, "userNameB", "email4@mail.ru", "password", Role.ROLE_ADMIN));
+            System.out.println(adminUserController.getByMail("email1@mail.ru"));
+            System.out.println(adminUserController.getAll());
+            System.out.println(adminUserController.get(1));
+            adminUserController.delete(1);
         }
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,65 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map <Integer, User> repository = new ConcurrentHashMap <>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return repository.remove(id).getId() == id;
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("save {}", user);
+        if (user.isNew()) {
+            Optional <User> userOptional = repository.values().stream()
+                    .filter(user1 -> user1.getEmail().equals(user.getEmail()))
+                    .findFirst();
+            if (!userOptional.isPresent()) {
+                user.setId(counter.incrementAndGet());
+                return repository.put(user.getId(), user);
+            }
+        }
+        return repository.computeIfPresent(user.getId(), (integer, oldUser) -> user);
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List <User> getAll() {
+        log.info("getAll");
+        return repository.values().stream()
+                .sorted(Comparator.comparing(AbstractNamedEntity::getName))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        return repository.values().stream()
+                .filter(user -> user.getEmail().equals(email))
+//                .findFirst().orElseThrow(() -> new NotFoundException("bad request"));
+        .findAny().orElse(null);
+    }
+}
