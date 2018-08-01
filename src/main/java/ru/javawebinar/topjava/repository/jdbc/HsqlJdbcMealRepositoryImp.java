27cb4891b8326782c149f7345fce920fbbdffa62
Kirilo Lozitsky <bondxxl01@gmail.com>
2018-08-01 05:01:54 +0300

    HW05 Task 5 Optional

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqlJdbcMealRepositoryImp.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqlJdbcMealRepositoryImp.java
@@ -8,24 +8,24 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-@Profile(Profiles.POSTGRES_DB)
-public class JdbcMealRepositoryImpl extends AbstractJdbcMealRepositoryImpl{
-
-    public JdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+@Profile(Profiles.HSQL_DB)
+public class HsqlJdbcMealRepositoryImp extends AbstractJdbcMealRepositoryImpl {
+    public HsqlJdbcMealRepositoryImp(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         super(jdbcTemplate, namedParameterJdbcTemplate);
     }
-
+//    https://stackoverflow.com/questions/8992282/convert-localdate-to-localdatetime-or-java-sql-timestamp
     @Override
     public Meal save(Meal meal, int userId) {
         MapSqlParameterSource map = new MapSqlParameterSource()
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", Timestamp.valueOf(meal.getDateTime()))
                 .addValue("user_id", userId);
 
         if (meal.isNew()) {
@@ -47,7 +47,6 @@ public class JdbcMealRepositoryImpl extends AbstractJdbcMealRepositoryImpl{
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
+                ROW_MAPPER, userId, Timestamp.valueOf(startDate), Timestamp.valueOf(endDate));
     }
-
 }
